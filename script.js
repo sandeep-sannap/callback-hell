1ac36f8dbe2e9c54a72bf4266f4c1ceb4dc3ad87
sandeepsannap <sandeep50sannap@gmail.com>
2021-05-31 12:54:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+var d = document.getElementById("display");
+
+setTimeout(() => {
+    display.innerHTML = '10';
+    setTimeout(() => {
+        display.innerHTML = '9';
+        setTimeout(() => {
+            display.innerHTML = '8';
+            setTimeout(() => {
+                display.innerHTML = '7';
+                setTimeout(() => {
+                    display.innerHTML = '6';
+                    setTimeout(() => {
+                        display.innerHTML = '5';
+                        setTimeout(() => {
+                            display.innerHTML = '4';
+                            setTimeout(() => {
+                                display.innerHTML = '3';
+                                setTimeout(() => {
+                                    display.innerHTML = '2';
+                                    setTimeout(() => {
+                                        display.innerHTML = '1';
+                                        setTimeout(() => {
+                                            display.innerHTML = '0';
+                                            setTimeout(() => {
+                                                display.innerHTML = 'Happy Independence Day';
+                                        
+                                            },1000);
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                       },1000);
+                  },1000);
+             },1000);
+         },1000);
+      },1000);
+   },1000);
+});
